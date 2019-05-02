ec5caa36ffdcde4038f72abb366d65f29798fabf
Lukas <Lukas@DESKTOP-DIONOV5>
2019-05-02 13:02:12 +0200

    Projektdateien hinzufügen.

---
+++ Buchstaben tester/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buchstaben_tester
+{
+    class Program
+    {
+
+        /*
+            Schreiben Sie einen Ausdruck in C#, der feststellt, ob in einem String kein Buchstaben mehrfach vorkommt.
+
+            Es gibt eine Lösung, die in einem kurzen Ausdruck mit drei LINQ-Methoden auskommt.
+
+            Für die Lösung erstellen Sie bitte eine fachlich korrekte Erläuterung, wie und warum sie funktioniert.
+
+            Zu Übungszwecken können Sie probieren, wie die Lösung ohne LINQ aussieht. Das sind dann sehr viel mehr Zeilen.
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
