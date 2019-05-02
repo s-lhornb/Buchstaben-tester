a30566f092da89595fc8361662f8b0055c415aac
Lukas <Lukas@DESKTOP-DIONOV5>
2019-05-02 15:41:09 +0200

    Änderung der Kommentare.

--- Buchstaben tester/Program.cs
+++ Buchstaben tester/Program.cs
@@ -13,7 +13,17 @@ namespace Buchstaben_tester
         /*
             Nutzen Sie Hilfe (aus dem Internet usw.) bitte ausschließlich, um die LINQ-Methoden und die Technik von Lambdas zu verstehen.
 
-            Schreiben Sie einen Ausdruck in C#, der feststellt, ob in einem String kein Buchstaben mehrfach vorkommt.
+
+
+
+            Schreiben Sie einen Ausdruck in C#, der feststellt, ob in einem String kein Buchstaben mehrfach vorkommt. 
+
+            Ich habe es so ausgelegt, dass keine Sonderzeichen geprüft werden sollen und es für die Suche egal ist, ob es ein Klein- oder Großbuchstabe ist.
+
+
+
+
+
 
             Es gibt eine Lösung, die in einem kurzen Ausdruck mit drei LINQ-Methoden auskommt.
 
@@ -44,28 +54,30 @@ namespace Buchstaben_tester
         }
 
         /// <summary>
-        /// Benutzt nur LINQ, ähnlich aufgebaut wie SQL-Code, um herauszufinden, ob doppelte Buchstaben in einem String sind.
+        /// Benutzt nur LINQ, vergleichbar aufgebaut wie SQL-Code, um herauszufinden, ob doppelte Buchstaben in einem String sind.
         /// </summary>
         /// <param name="s">Zu testender String</param>
         /// <returns>Ergebnis, ob String keine doppelten Buchstaben hat: Ture = keine, False = mind. einen</returns>
         private static bool CharacterTesterLINQ1(string s)
         {
-            return (from char chars in Regex.Replace(s.ToLower(), @"[^a-z]", "") //übernimmt den String, welcher hier in Kleinbuchstaben umgewandelt und dann von allen nicht Kleinbuchstaben gereinigt wird. Das Entfernen der Sonderzeichen könnte auch mit einem Where-Filter durchgeführt werden.
+            return (from char chars in Regex.Replace(s.ToLower(), @"[^a-z]", "") //übernimmt den String, welcher hier in Kleinbuchstaben umgewandelt und dann von allen nicht Kleinbuchstaben gereinigt wird. 
+                                                                                 //Das Entfernen der Sonderzeichen könnte auch mit einem Where-Filter durchgeführt werden, welcher vor der group-Methode durchgeführt würde.
                     group chars by chars into chargroup  // Diese Methode gruppiert alle Buchstaben, bei denen der Buchstabe gleich ist in chargroup.
-                    where chargroup.Count() > 1 // Filtert alle Elemente aus Chargroup heraus, die die Bedingung nicht erfüllen, also nicht eine größere Anzahl als 1 haben.
+                    where chargroup.Count() > 1 // Filtert alle Elemente aus chargroup heraus, die die Bedingung nicht erfüllen, also nicht eine größere Anzahl als 1 haben.
                     select chargroup) // Gibt den Typ der zurückgegeben Elemente an. Also alle von chargroup, die nicht gefiltert wurden.
-                    .Count() //Zählt alle Elemente, die im LINQ-Teil davor gefunden wurden.
+                    .Count() // Zählt alle Elemente (chargroup), die im LINQ-Teil davor gefunden wurden.
                     .Equals(0); // Überprüft, ob die Anzahl der Elemente gleich 0 ist.
         }
 
         /// <summary>
-        /// Benutzt LINQ und Lambdas, ähnlich aufgebaut wie Streams mit Lambdas in Java-Code, um herauszufinden, ob doppelte Buchstaben in einem String sind.
+        /// Benutzt LINQ und Lambdas, vergleichbar aufgebaut wie Streams mit Lambdas in Java-Code, um herauszufinden, ob doppelte Buchstaben in einem String sind.
         /// </summary>
         /// <param name="s">Zu testender String</param>
         /// <returns>Ergebnis, ob String keine doppelten Buchstaben hat: Ture = keine, False = mind. einen</returns>
         private static bool CharacterTesterLINQ2(string s)
         {
-            return Regex.Replace(s.ToLower(), @"[^a-z]", "") //Der String wird in den lower Case geschrieben und es werden alle Elemente herausgefiltert, die keine Kleinbuchstaben sind. Das Entfernen der Sonderzeichen könnte nach der Umwandlung in ein Array auch mit einem Where-Filter durchgeführt werden.
+            return Regex.Replace(s.ToLower(), @"[^a-z]", "") //Der String wird in den lower Case geschrieben und es werden alle Elemente herausgefiltert, die keine Kleinbuchstaben sind. 
+                                                             //Das Entfernen der Sonderzeichen könnte nach der Umwandlung in ein Array auch mit einem Where-Filter durchgeführt werden.
                 .ToCharArray() //Der String wird in ein Character-Array umgewandelt.
                 .GroupBy(chars => chars) //Alle gleiche Buchstaben werden gruppiert.
                 .Where(chargroup => chargroup.Count() > 1) //Alle Gruppen, die nicht mehr als 1 Element haben werden hier herausgefiltert.
